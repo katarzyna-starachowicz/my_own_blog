cf29c026d47d4ff7414e42741d93af3340d8f4a0
katarzyna-starachowicz <kasia@x8.io>
2016-11-15 14:39:43 +0100

    Implement posts#show action with views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,12 @@
 class PostsController < ApplicationController
   def index
     posts = repo.all_from_the_last
-
     render 'posts/index', locals: { posts: posts }
+  end
+
+  def show
+    post = repo.find(params[:id])
+    render 'posts/show', locals: { post: post }
   end
 
   private
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe PostsController, type: :controller do
 
     it_behaves_like 'template rendering action', :index
   end
+
+  describe "GET #show" do
+    subject    { get :show, id: post.id }
+    let(:post) { create(:post) }
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it_behaves_like 'template rendering action', :show
+  end
 end
