7c047cf9777ca4052b6bd6a9142e8e84c0db22a8
katarzyna-starachowicz <kasia@x8.io>
2016-11-17 12:03:05 +0100

    Refactor posts query test

--- spec/queries/posts_query_spec.rb
+++ spec/queries/posts_query_spec.rb
@@ -4,13 +4,11 @@ RSpec.describe PostsQuery do
   subject(:posts_query) { described_class.new }
 
   describe '#all_from_the_last' do
-    let(:post_form) { PostForm.new(title: 'AA', body: 'aa') }
-    let(:repo)      { PostsRepo.new }
-    let!(:post_1)   { repo.create(post_form) }
-    let!(:post_2)   { repo.create(post_form) }
+    let!(:post_1)   { create(:post) }
+    let!(:post_2)   { create(:post) }
 
     it 'returns all posts in descending order' do
-      returned_ids = posts_query.all_from_the_last.map(&:id)
+      returned_ids = posts_query.all_from_the_last.pluck(:id)
 
       expect(returned_ids).to eq([post_2.id, post_1.id])
     end
